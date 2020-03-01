a61d2cffba612bc40821651a0d21ed3d0d3d8c2f
Chris Ohk <utilForever@gmail.com>
2020-03-01 10:48:53 +0900

    std::format 예제 코드 추가

---
+++ 5 - Format/type_safety.cpp
@@ -0,0 +1,26 @@
+// Run with in cl
+/*
+cl.exe hello_world.cpp /I fmt/include /std:c++latest /EHsc /MD /link fmt.lib
+*/
+
+#include <fmt/format.h>
+
+#include <iostream>
+
+int main()
+{
+    // Part 1
+    // Throw format_error exception: "unknown format code 'd' for string".
+    fmt::format("The answer is {:d}\n", "forty-two");
+
+    // Report a compile-time error for the same reason
+    //fmt::format(FMT_STRING("The answer is {:d}\n"), "forty-two");
+
+    // Part 2
+    // Report a compile-time error because wide character L'\x42e' cannot be formatted into a narrow string
+    fmt::format("Cyrillic letter {}\n", L'\x42e');
+
+    // fmt::format(L"Cyrillic letter {}\n", L'\x42e');
+
+    return 0;
+}
